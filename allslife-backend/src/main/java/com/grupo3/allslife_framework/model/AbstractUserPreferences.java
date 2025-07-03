42c2315a57f95bbab6c455fc5d5b94ed6c4c9afa
gili-julio <giliardoplay@gmail.com>
2025-07-03 06:51:26 -0300

    remove unused import

--- allslife-backend/src/main/java/com/grupo3/allslife_framework/model/AbstractUserPreferences.java
+++ allslife-backend/src/main/java/com/grupo3/allslife_framework/model/AbstractUserPreferences.java
@@ -1,7 +1,6 @@
 package com.grupo3.allslife_framework.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.*;
 import lombok.Data;
